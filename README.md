8672f3febdbb67f251966f66e6fbeadc49d1b530
Adam Trojanowski <troj.adev@gmail.com>
2026-03-28 11:28:31 +0100

    Rewrite audio-processor.py script logic

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Audio Alchemist CLI
 
-A Python-based utility designed to automate the normalization of .wav audio files using the SoX (Sound eXchange) library. This tool processes audio files to a peak level of -3dB and generates a JSON-formatted summary of the processed files, including duration and channel configuration.
+A Python-based utility designed to automate processing .wav audio files using the SoX (Sound eXchange) library. This tool processes audio files adding bunch of audio FX with random parameters and generates a JSON-formatted summary of the processed files, including duration and channel configuration.
+
+By default program outputs 3 processed variations of source file.
--- audio-processor.py
+++ audio-processor.py
@@ -1,54 +1,37 @@
 import os
+import random
 import subprocess
 import json
 import sys
 
 
-def normalize_audio_file(filepath):
+def randomize_parameters():
+    
+    #Randomize pitch between -1 to + 1 octave
+    pitch = random.randint(-1200, 1200)
 
-    parent_directory = os.path.dirname(filepath)
-    filename = os.path.basename(filepath)
-    filepath_splitted = os.path.splitext(filepath)
+    #Randomoze tempo factor
+    tempo_factor = random.uniform(0.2, 1.0)
+    #Randomize stretch factor
+    stretch_factor = random.uniform(0.2, 1.0)
+    #Randomize reverberance, high frequecies damping, pre-delay
+    reverberance = random.randint(25, 100)
+    hf_damping = random.randint(10, 60)
+    pre_delay = random.randint(0, 300)
 
-    print(filepath)
-    print(filepath_splitted)
-    processed_files_directory = f"{parent_directory}/processed_files/"
+    #Create effect list with parameters for further use in SoX command run using subprocess module
+    effects_list = [
+        "pitch", pitch,
+        "tempo", tempo_factor,
+        "reverb", reverberance, hf_damping, 100, 100, pre_delay, 0,
+        "stretch", stretch_factor
 
-    # Create a save path for a processed file
-    filepath_new = processed_files_directory + \
-        f"{os.path.basename(filepath_splitted[0])}_normalized{filepath_splitted[1]}"
+    ]
 
-    try:
-
-        # Check if directory for saving processed files exists
-
-        if not os.path.isdir(processed_files_directory):
-            os.mkdir(processed_files_directory)
-
-        # Normalize audio file and save it to processed files directory
-
-        subprocess.run(["/usr/bin/sox", filepath,
-                       filepath_new, "norm", "-3"], check=True)
-
-        duration = subprocess.check_output(
-            ["/usr/bin/soxi", "-d", filepath], text=True).replace("\n", "")
-        channels = subprocess.check_output(
-            ["/usr/bin/soxi", "-c", filepath], text=True).replace("\n", "")
-
-        print(f"{filename} normalized!")
-
-        return {
-            "filename:": filename,
-            "length:": duration,
-            "channels:": channels,
-            "success:": "success"
-
-        }
-    except subprocess.CalledProcessError as e:
-        print(f"Error processing file: {e}", file=sys.stderr)
+    return [str(e) for e in effects_list if e]
 
 
-def batch_normalize_files(parent_directory):
+def batch_edit_files(parent_directory):
 
     processed_files_directory = f"{parent_directory}/processed_files/"
     processed_audio_data_list = []
@@ -61,24 +44,26 @@ def batch_normalize_files(parent_directory):
             if not os.path.isdir(processed_files_directory):
                 os.mkdir(processed_files_directory)
 
-            filepath = f"{parent_directory}/{file}"
-            filepath_new = processed_files_directory + \
-                f"{os.path.splitext(file)[0]}_processed{os.path.splitext(file)[1]}"
+            for i in range(1, 4):
 
-            # Normalize audio file and save it to processed files directory
+                filepath = f"{parent_directory}/{file}"
+                filepath_new = processed_files_directory + \
+                    f"{os.path.splitext(file)[0]}_processed_{i}{os.path.splitext(file)[1]}"
 
-            subprocess.run(["/usr/bin/sox", "-V 0", filepath,
-                           filepath_new, "norm", "-3"], check=True)
+                # Normalize audio file and save it to processed files directory
 
-            duration = subprocess.check_output(
-                ["/usr/bin/soxi", "-V 0", "-d", f"{filepath}"], text=True).replace("\n", "")
-            channels = subprocess.check_output(
-                ["/usr/bin/soxi", "-V 0", "-c", f"{filepath}"], text=True).replace("\n", "")
+                subprocess.run(["/usr/bin/sox", "-V 0", filepath,
+                                filepath_new] + randomize_parameters(), check=True)
 
-            processed_audio_data_list.append({"filename:": file,
-                                              "length:": duration,
-                                              "channels:": channels,
-                                              "success:": "success"})
+                duration = subprocess.check_output(
+                    ["/usr/bin/soxi", "-V 0", "-d", f"{filepath}"], text=True).replace("\n", "")
+                channels = subprocess.check_output(
+                    ["/usr/bin/soxi", "-V 0", "-c", f"{filepath}"], text=True).replace("\n", "")
+
+                processed_audio_data_list.append({"filename:": file,
+                                                  "length:": duration,
+                                                  "channels:": channels,
+                                                  "success:": "success"})
 
     return processed_audio_data_list
 
@@ -86,24 +71,12 @@ def batch_normalize_files(parent_directory):
 def main():
     directory = "/data"
 
-    # Below code will be used after I write possibility to either batch process or provide specific file as an argument
-    # #Check if any argument was provided
-    # if len(sys.argv) == 2:
-    #     filepath = sys.argv[1]
-    #     if not os.path.exists(filepath):
-    #         print(f"Error: File {filepath} does not exist.", file=sys.stderr)
-    #         sys.exit(1)
-    # elif len(sys.argv) > 2:
-    #     print(f"Passed {len(sys.argv) - 1} arguments, but only 1 needed", file=sys.stderr)
-    #     sys.exit(1)
-    # else:
-    #     print("Filepath not specified, please provide one.", file=sys.stderr)
-    #     sys.exit(1)
-
     try:
         print(f"Current python version: {sys.version}")
+
         # Normalize audio, retrieve files info and save it in JSON format
-        audio_data = batch_normalize_files(directory)
+
+        audio_data = batch_edit_files(directory)
         audio_data_json = json.dumps(audio_data, indent=4)
         print(audio_data_json)
         sys.exit(0)
